7b10f787719cca9c3a145d0e682068833303c4c5
YudanovRoman <156830559+YudanovRoman@users.noreply.github.com>
2025-01-25 10:15:25 +0300

    Вот код с которым можно работать

--- main.py
+++ main.py
@@ -7,199 +7,209 @@ class OfficeCrusher:
     def __init__(self, player):
         pygame.init()
 
-        self.size = self.weight, self.height = 800, 600 # задаем размеры
-        self.screen = pygame.display.set_mode(self.size)
+        self.screen = pygame.display.set_mode((1024, 700))
+        self.screen.fill("black")
         pygame.display.set_caption('Office Crusher')
-
         self.clock = pygame.time.Clock()
 
         self.flag_game = False
         self.flag_main_menu = True
-
-        self.in_process = True
-
-        self.board = Board()  # Инициализация класса Board
-        self.menu = MainMenu(self.screen)
+        self.flag_controls_menu = False
+        self.board = Board(player)  # Инициализация класса Board
         self.player = player
 
     def run(self):
-        while self.in_process:
+        while True:
             if self.flag_main_menu:
-                self.menu.update()
-            if self.flag_game:
-                self.update() # обновление экрана помешено в одну функцию (это можно обсудить)
+                self.main_menu()
+            elif self.flag_controls_menu:
+                self.controls_menu()
             self.handle_events()
+            if self.flag_game:
+                self.update()
+                self.render()  # Отрисовка экрана
 
             self.clock.tick(60)  # Ограничение до 60 FPS
+
+    def main_menu(self):
+        self.screen.fill("black")
+
+        # Отображение текста вместо логотипа
+        font = pygame.font.SysFont(None, 48)
+        text_surface = pygame.image.load("лого (1).png").convert_alpha()
+        text_rect = text_surface.get_rect(center=(500, 100))  # Центрирование текста
+        self.screen.blit(text_surface, text_rect)
+
+        # Загрузка текстур кнопок
+        self.button_start_texture = pygame.image.load("начать (1).png").convert_alpha()
+        self.button_option_texture = pygame.image.load("Управление (1).png").convert_alpha()
+        self.button_exit_texture = pygame.image.load("уровни (1).png").convert_alpha()
+
+        # Создание и отображение кнопок
+        self.button_start_rect = self.button_start_texture.get_rect(topleft=(20, 250))
+        self.screen.blit(self.button_start_texture, self.button_start_rect.topleft)
+
+        self.button_option_rect = self.button_option_texture.get_rect(topleft=(20, 402))
+        self.screen.blit(self.button_option_texture, self.button_option_rect.topleft)
+
+        self.button_exit_rect = self.button_exit_texture.get_rect(topleft=(20, 700))
+        self.screen.blit(self.button_exit_texture, self.button_exit_rect.topleft)
+
+        pygame.display.flip()
+
+    def controls_menu(self):
+        self.screen.fill("black")
+
+        # Открытие изображения на весь экран
+        controls_image = pygame.image.load("controls_image.png").convert_alpha()  # Замените на ваше изображение
+        controls_image = pygame.transform.scale(controls_image, (1480, 1024))
+        self.screen.blit(controls_image, (0, 0))
+
+        # Кнопка выхода назад
+        self.button_back_texture = pygame.image.load("кнопка_назад.png").convert_alpha()
+        self.button_back_rect = self.button_back_texture.get_rect(topleft=(20, 700))
+        self.screen.blit(self.button_back_texture, self.button_back_rect.topleft)
+
+        pygame.display.flip()
 
     def handle_events(self):
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.in_process = False
+                pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                self.flag_game = False
-                self.flag_main_menu = True
-
-        if self.menu.get_in_game():
-            self.flag_game = True
-            self.flag_main_menu = False
-        if self.menu.get_stop_process():
-            self.in_process = False
-            sys.exit()
-
-        if self.flag_game:
-            if keys[pygame.K_LCTRL]:
-                self.player.speed_x = self.player.speed_y = 5
-            else:
-                self.player.speed_x = self.player.speed_y = 3
-            if keys[pygame.K_LEFT]:
-                if self.player.position[0] >= 0:
-                    self.player.position[0] -= self.player.speed_x
-                    self.player.sprite_player.rect.x -= self.player.speed_x
-            if keys[pygame.K_RIGHT]:
-                if self.player.position[0] <= 700:
-                    self.player.position[0] += self.player.speed_x
-                    self.player.sprite_player.rect.x += self.player.speed_x
-            if keys[pygame.K_UP]:
-                if self.player.position[1] >= 0:
-                    self.player.position[1] -= self.player.speed_y
-                    self.player.sprite_player.rect.y -= self.player.speed_y
-            if keys[pygame.K_DOWN]:
-                if self.player.position[1] <= 425:
-                    self.player.position[1] += self.player.speed_y
-                    self.player.sprite_player.rect.y += self.player.speed_y
+            if hasattr(self, 'button_start_rect'):
+                if self.button_start_rect.collidepoint(pygame.mouse.get_pos()) and self.flag_main_menu and \
+                        pygame.mouse.get_pressed()[0]:
+                    self.flag_game = True
+                    self.flag_main_menu = False
+                if self.button_option_rect.collidepoint(pygame.mouse.get_pos()) and self.flag_main_menu and \
+                        pygame.mouse.get_pressed()[0]:
+                    self.flag_controls_menu = True
+                    self.flag_main_menu = False
+                if self.button_exit_rect.collidepoint(pygame.mouse.get_pos()) and self.flag_main_menu and \
+                        pygame.mouse.get_pressed()[0]:
+                    pygame.quit()
+                    sys.exit()
+            if hasattr(self, 'button_back_rect'):
+                print(self.button_back_rect)
+                if self.flag_controls_menu and self.button_back_rect.collidepoint(pygame.mouse.get_pos()) and \
+                        pygame.mouse.get_pressed()[0]:
+                    self.flag_controls_menu = False
+                    self.flag_main_menu = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.flag_game = False
+                    self.flag_main_menu = True
+                if self.flag_game:
+                    y = self.board.level_data.index([i for i in self.board.level_data if "@" in i][0])
+                    x = [i for i in self.board.level_data if "@" in i][0].index("@")
+                    if event.key == pygame.K_LEFT:
+                        self.player.sprite_player.image = pygame.transform.rotate(
+                            pygame.image.load("персонаж_вниз.png"), 270)
+                        if x > 0 and self.board.level_data[y][x - 1] == ".":
+                            self.board.level_data[y][x - 1] = "@"
+                            self.board.level_data[y][x] = "."
+                            self.move("x", -5)
+                    if event.key == pygame.K_RIGHT:
+                        self.player.sprite_player.image = pygame.transform.rotate(
+                            pygame.image.load("персонаж_вниз.png"), 90)
+                        if x < len(self.board.level_data[y]) and self.board.level_data[y][x + 1] == ".":
+                            self.board.level_data[y][x + 1] = "@"
+                            self.board.level_data[y][x] = "."
+                            self.move("x", 5)
+                    if event.key == pygame.K_UP:
+                        self.player.sprite_player.image =\
+                            pygame.transform.rotate(pygame.image.load("персонаж_вниз.png"), 180)
+                        if x > 0 and self.board.level_data[y - 1][x] == ".":
+                            self.board.level_data[y - 1][x] = "@"
+                            self.board.level_data[y][x] = "."
+                            self.move("y", -5)
+                    if event.key == pygame.K_DOWN:
+                        self.player.sprite_player.image = pygame.image.load("персонаж_вниз.png")
+                        if y < len(self.board.level_data) and self.board.level_data[y + 1][x] == ".":
+                            self.board.level_data[y + 1][x] = "@"
+                            self.board.level_data[y][x] = "."
+                            self.move("y", 5)
 
     def update(self):
-        self.screen.fill((255, 255, 255))
-        self.board.update()  # Обновление состояния объекта Board
-        self.player.update(self.screen)  # Очистка экрана
-        # self.board.render(self.screen)
+        self.player.update(self.screen)
         pygame.display.flip()
 
-
-class Board:
-    def __init__(self):
-        self.width = "заглушка"  # позже напишем логику где многое будет импортированно из файла уровня
-        self.height = "заглушка"
-        self.left = "заглушка"
-        self.top = "заглушка"
-        self.cell_size = "заглушка"
-
-    def update(self):
-        pass
-
-    def render(self, screen):
-        for row in range(self.height):
-            for col in range(self.width):
-                pygame.draw.rect(screen, 'white', (self.left + self.cell_size * col,
-                                                   self.top + self.cell_size * row,
-                                                   self.cell_size, self.cell_size), width=1)
+    def render(self):
+        self.screen.fill((255, 255, 255))  # Очистка экрана
+        self.board.render(self.screen)
 
 
-class MainMenu:
-    def __init__(self, screen):
-        self.screen = screen
-        self.mousePos = pygame.mouse.get_pos()
-        self.is_clicked = pygame.mouse.get_pressed(num_buttons=3)[0]
-
-        self.in_game = False
-        self.stop_process = False
-
-        self.label = pygame.font.Font("BlackOpsOne-Regular_RUS_by_alince.otf", 30)
-        self.start_button = Button(self.screen, 30, 30, 300, 100, self.label, 'startButton',
-                                   'Начать игру', 'green')
-        self.exit_button = Button(self.screen, 30, 200, 300, 100, self.label, 'exitButton',
-                                   'Выйти из игры', 'red')
-        self.buttons = [self.start_button, self.exit_button]
-
-    def update(self):
-        self.mousePos = pygame.mouse.get_pos()
-        self.is_clicked = pygame.mouse.get_pressed(num_buttons=3)[0]
-
-        self.screen.fill('black')
-
-        for button in self.buttons:
-            clicked_button = button.update(self.mousePos, self.is_clicked)
-            if clicked_button is not None:
-                if clicked_button == 'startButton':
-                    print('start button pressed!')
-                    self.in_game = True
-                elif clicked_button == 'exitButton':
-                    print('exit button pressed!')
-                    self.stop_process = True
-
-        pygame.display.flip()
-
-    def get_stop_process(self):
-        return self.stop_process
-
-    def get_in_game(self):
-        return self.in_game
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y, list_level):
+        super().__init__()
+        num_x = 512 - int(max([len(i) for i in list_level]) / 2 * 50)
+        num_y = 350 - len(list_level) / 2 * 50
+        if tile_type == 'empty':
+            self.image = pygame.image.load("тайл пол.png").convert_alpha()
+        elif tile_type == "furniture":
+            self.image = pygame.image.load("мебель_тайл_1.png").convert_alpha()
+        self.rect = self.image.get_rect().move(
+            50 * pos_x + num_x, 50 * pos_y + num_y)
 
 
 class Player:
-    def __init__(self):
-        self.position = [100, 100]  # Начальная позиция игрока
-        self.speed_x = 3
-        self.speed_y = 3
+    def __init__(self, x_c, y_x):
         self.sprite_player_group = pygame.sprite.Group()
-        self.sprite_player = pygame.sprite.Sprite(self.sprite_player_group)
-        self.sprite_player.image = pygame.image.load("men.png")
+        self.sprite_player = pygame.sprite.Sprite()
+        self.sprite_player.image = pygame.image.load("персонаж_вниз.png")
         self.sprite_player.rect = self.sprite_player.image.get_rect()
-        self.sprite_player.rect.x = 100
-        self.sprite_player.rect.y = 100
+        self.sprite_player_group.add(self.sprite_player)
+        print(x_c, " sd")
+        self.sprite_player.rect.x = x_c  # Начальная позиция игрока
+        self.sprite_player.rect.y = y_x
 
     def update(self, screen):
         self.sprite_player_group.draw(screen)
 
 
-class Button:
-    def __init__(self, screen, x, y, width, height, label, name, text='Button', text_color='black'):
-        self.screen = screen
-        self.label = label
-        self.name = name
-        self.x, self.y = x, y
-        self.width, self.height = width, height
-        self.text_image = self.label.render(text, True, text_color)
-        self.text_image_rect = self.text_image.get_rect()
+class Board:
+    def __init__(self, player):
+        self.tiles = pygame.sprite.Group()  # Группа спрайтов для тайлов
+        self.width = 0
+        self.height = 0
+        self.player = player
+        self.cell_size = 50  # Размер ячейки (50px)
+        self.level_data = [list(i) for i in self.load_level("level.txt")]  # Пример имени файла
+        self.load_level_data()  # Загрузка уровня (получите уровень через метод)
 
-        self.back_colors = {
-            'normal': (30, 30, 30),
-            'hover': (100, 100, 100),
-            'clicked': (200, 200, 200)
-        }
+    def load_level_data(self):
+        self.width, self.height = self.generate_level(self.level_data)
 
-        self.button_surface = pygame.Surface((self.width, self.height))
-        self.button_rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.text_image_rect.center = self.width / 2, self.height / 2
+    def load_level(self, filename):
+        filename = "data/" + filename
+        with open(filename, 'r') as mapFile:
+            level_map = [line.strip() for line in mapFile]
 
-        self.is_pressed = False
+        max_width = max(map(len, level_map))
+        return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
-    def update(self, mouse_pos, is_clicked):
-        res = False
+    def generate_level(self, level):
+        x, y = 0, 0
+        for y in range(len(level)):
+            for x in range(len(level[y])):
+                tile = Tile('empty', x, y, level)
+                self.tiles.add(tile)  # Добавление тайла в группу
+                if level[y][x] == '#':
+                    tile = Tile('furniture', x, y, level)
+                    self.tiles.add(tile)  # Добавление тайла в группу
+                elif level[y][x] == '@':
+                    self.player.sprite_player.rect.x = \
+                        512 - int(max([len(i) for i in level]) / 2 * 50) + x * 50
+                    self.player.sprite_player.rect.y = \
+                        int(350 - len(level) / 2 * 50 + y * 50)
+        return x, y
 
-        if self.button_rect.collidepoint(mouse_pos) and is_clicked:
-            self.button_surface.fill(self.back_colors['clicked'])
-            self.is_pressed = True # состояние зажатия кнопки
-        elif self.button_rect.collidepoint(mouse_pos):
-            if self.is_pressed: # если кнопку кликнули (зажали и отпустили)
-                res = True
-                self.is_pressed = False
-            else:
-                self.button_surface.fill(self.back_colors['hover'])
-        else:
-            self.is_pressed = False
-            self.button_surface.fill(self.back_colors['normal'])
-
-        self.button_surface.blit(self.text_image, self.text_image_rect) # отображение текста на поверхности кнопки
-        self.screen.blit(self.button_surface, self.button_rect) # отображение поверхности кнопки на экране
-
-        if res:
-            return self.name
+    def render(self, screen):
+        self.tiles.draw(screen)  # Отображение всех спрайтов в группе
 
 
 if __name__ == "__main__":
-    game = OfficeCrusher(Player())
+    game = OfficeCrusher(Player(0, 0))
     game.run()
